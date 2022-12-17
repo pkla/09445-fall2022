948b77b99ba3d3c6f4656be945917b62832ff66a
Sage Betko <sage.betko@gmail.com>
2022-12-17 13:37:57 -0500

    Add setup.py for making DFTBrepulsive pip-installable

---
+++ DFTBrepulsive_package/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+# setup(
+#     name="DFTBrepulsive",
+#     version="1.0",
+#     package_dir={"DFTBrepulsive": "."},
+#     packages=["DFTBrepulsive"],
+# )
+
+
+setup(
+    name="DFTBrepulsive",
+    version="1.0",
+    packages=find_packages("DFBrepulsive"),
+)
